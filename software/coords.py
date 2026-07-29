29a95d885e3736a6b5b59b4c63f8e4522e63f403
mint-garrett <stmichaelboy@icloud.com>
2026-07-29 19:21:31 -0400

    introduces early logic in update_degree_fn to append degree vector with degree values

--- software/coords.py
+++ software/coords.py
@@ -1,14 +1,28 @@
 import numpy
-from movement_functions import MOTORS
+from config import MOTORS
 
 def degree_vector_init(): 
-    degree_vector = numpy.array([0],[0],[0],[0])
-            return degree_vector
+    degree_vector = numpy.matrix([[0],[0],[0],[0]])
+    return degree_vector
 
-def update_degree_vector(degree_vector, servo_number, current_wf_pos, last_wf_pos):
+def update_degree_vector(degree_vector, current_wf_pos, last_wf_pos):
     #magical code
     #TODO: get degree values from waveform timing, write them into the degree vector
-    return updated_degree_vector
+     
+    for name, m in MOTORS.items():
+        if m["wf_pos"] != m["old_wf_pos"]:
+            sum = m["wf_pos"] - m["old_wf_pos"]
+            if sum == -20:
+                print("-20")
+            elif sum == 20:
+                print("20")
+            else:
+                print(f"{sum}")
+        else:
+            print("no change")
+
+
+    #return updated_degree_vector
 
 def triangulate(updated_degree_vector, servo_number):
     #TODO: take degree vector, and turn it into a system that can figure out where the finger is
